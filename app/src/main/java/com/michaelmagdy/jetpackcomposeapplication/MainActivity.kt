642f868bf72391e19697f22a6c1cffcec701c324
MichaelTheBestProgrammerInTheWorld <michaelmagdy_93@yahoo.com>
2022-01-31 14:24:59 +0200

    Basic column attributes

--- app/src/main/java/com/michaelmagdy/jetpackcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/michaelmagdy/jetpackcomposeapplication/MainActivity.kt
@@ -3,10 +3,17 @@ package com.michaelmagdy.jetpackcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.michaelmagdy.jetpackcomposeapplication.ui.theme.JetpackComposeApplicationTheme
 
@@ -14,25 +21,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            Column(modifier = Modifier.background(Color.Green)
+                .fillMaxSize(0.5f),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "Hello")
+                Text(text = "Android")
+                Text(text = "World")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeApplicationTheme {
-        Greeting("Android JetpackCompose")
-    }
-}
