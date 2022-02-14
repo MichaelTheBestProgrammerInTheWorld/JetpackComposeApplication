9936993cba29c8f798bf2e8594cbe563ae7110d6
MichaelTheBestProgrammerInTheWorld <michaelmagdy_93@yahoo.com>
2022-02-14 21:27:54 +0200

    ConstraintLayout

--- app/src/main/java/com/michaelmagdy/jetpackcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/michaelmagdy/jetpackcomposeapplication/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -33,6 +34,9 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.michaelmagdy.jetpackcomposeapplication.ui.theme.JetpackComposeApplicationTheme
 import kotlinx.coroutines.launch
 import kotlin.random.Random
@@ -41,19 +45,36 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            
-            LazyColumn {
 
-                items(count = 5000){
-                    Text(text = "Item $it",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 24.dp)
-                    )
+            val constraints = ConstraintSet {
+                val greenBox = createRefFor("greenBox")
+                val redBox = createRefFor("redBox")
+
+                constrain(greenBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
                 }
+
+                constrain(redBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                //createHorizontalChain(greenBox, redBox)
+            }
+
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+
+                Box(modifier = Modifier.background(Color.Green)
+                    .layoutId("greenBox"))
+
+                Box(modifier = Modifier.background(Color.Red)
+                    .layoutId("redBox"))
             }
         }
     }
